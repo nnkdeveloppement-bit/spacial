40ae9d541c8f16264e93ac8ae5b1f14979226a63
NNK <nakabi.naima@gmail.com>
2026-02-26 20:07:42 +0000

    mise ajour des requierents.txt

--- app.py
+++ app.py
@@ -1,55 +1,35 @@
 import streamlit as st
-import geemap as geemap
-import folium
-from streamlit_folium import st_folium
+import geemap.foliumap as geemap
 from processing import SpatialProcessor
-st.cache_data.clear()
-st.cache_resource.clear()
 
 st.title("Mon Premier Portail GEE")
 
-# 1. Connexion au moteur métier
 @st.cache_resource
 def load_engine():
     return SpatialProcessor()
 
 engine = load_engine()
 
-# 2. Formulaire de saisie
 with st.sidebar:
     st.header("Coordonnées")
     lat = st.number_input("Latitude", value=48.85, format="%.4f")
     lon = st.number_input("Longitude", value=2.35, format="%.4f")
     submit = st.button("Afficher la zone")
 
-
-# 3. Affichage de la carte (Version STABLE)
+m = geemap.Map(center=[lat, lon], zoom=12)
 
 if submit:
-
     with st.spinner("Récupération de l'image depuis le Cloud..."):
-
-        # Image Earth Engine
         img = engine.get_satellite_image(lat, lon)
 
-        # Paramètres d'affichage
         vis_params = {
-            'bands': ['B4', 'B3', 'B2'],
-            'min': 0,
-            'max': 3000
+            'bands': ['B4','B3','B2'],
+            'min':0,
+            'max':3000
         }
 
-        # Création carte Folium
-        m = folium.Map(location=[lat, lon], zoom_start=12)
-
-        # Ajout image Sentinel
-        geemap.ee_tile_layer(
-            img,
-            vis_params,
-            "Sentinel-2"
-        ).add_to(m)
-
-        # Affichage stable Streamlit
-        st_folium(m, height=600, width=700)
+        m.addLayer(img, vis_params, 'Sentinel-2')
 
         st.success(f"Image chargée pour {lat}, {lon}")
+
+st.components.v1.html(m.to_html(), height=600)
